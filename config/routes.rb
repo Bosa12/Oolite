4dc6f35a2e97418f2c4d21b029feeeabe87dc3bf
catriona beamish <riccibozza@gmail.com>
2024-05-22 00:38:45 +0100

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
  get "home/index"
- get 'pages/index', to: 'pages#index'
+ get 'pages', to: 'pages#home'
 end
