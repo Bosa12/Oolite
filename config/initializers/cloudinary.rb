4dbf28d0da2bbcd97bafee6a15a1f4911c50e6b5
catriona beamish <riccibozza@gmail.com>
2024-06-30 13:22:51 +0100

    cloudi

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,7 +1,7 @@
 Cloudinary.config do |config|
-  config.cloud_name = ENV['dfp6xzba7']
+  config.cloud_name = ENV['dc6nbm8zp']
   config.api_key = ENV['837124733572619']
-  config.api_secret = ENV['STUFeCCL44fU03RXxlg9lgP9Ab0']
+  config.api_secret = ENV['EZP6yNgJ9z6bUjuWY5ouwdmmc5U']
   config.secure = true
   config.cdn_subdomain = true
 end
