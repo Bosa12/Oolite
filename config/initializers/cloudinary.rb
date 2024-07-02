7235bc588c1d4411be226fa6bdb57cd6475c9bb2
catriona beamish <riccibozza@gmail.com>
2024-07-02 22:21:02 +0100

    projservice

--- config/initializers/cloudinary.rb
+++
@@ -1,7 +0,0 @@
-Cloudinary.config do |config|
-  config.cloud_name = ENV['dc6nbm8zp']
-  config.api_key = ENV['837124733572619']
-  config.api_secret = ENV['EZP6yNgJ9z6bUjuWY5ouwdmmc5U']
-  config.secure = true
-  config.cdn_subdomain = true
-end
