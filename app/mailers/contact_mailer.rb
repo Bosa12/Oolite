25cc19322523cbad0eb12fd3b7ea54ba290d7d37
catriona beamish <riccibozza@gmail.com>
2024-07-18 12:22:56 +0100

    update

--- app/mailers/contact_mailer.rb
+++
@@ -1,8 +0,0 @@
-class ContactMailer < ApplicationMailer
-  default from: "oolite.uk.co"
-
-  def contact_email(contact)
-    @contact = contact
-    mail(to: "email address", subject: "New Contact Message")
-  end
-end
