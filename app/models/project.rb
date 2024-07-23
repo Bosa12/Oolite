bc246a0d1eb28feb36598edac304842681c3f4dd
catriona beamish <riccibozza@gmail.com>
2024-07-23 23:25:43 +0100

    modificado a chamada do frontend para o activestorage/cloudinary

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,12 +2,7 @@ class Project < ApplicationRecord
   belongs_to :project_type
   has_many :images, dependent: :destroy
   has_many_attached :images
-  # validates :name, presence: true
-  # validate :images_presence
-  # validates :name, :description, :project_type, presence: true
-  # validates :attribute, presence: true
-
-
+  
   def images_presence
     errors.add(:images, "can't be blank") if images.empty?
   end
